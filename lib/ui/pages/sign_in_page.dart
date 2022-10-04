6e4b044c08df4d81e69f7e5ce273a364ab406039
seshadwi <seshaadwi@gamil.com>
2022-10-04 15:47:31 +0700

    slicing sign in page 2

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -17,7 +17,7 @@ class SignInPage extends StatelessWidget {
               height: 49,
               width: 191,
               margin: const EdgeInsets.only(
-                top: 100,
+                top: 65,
                 bottom: 100,
               ),
               decoration: const BoxDecoration(
@@ -41,9 +41,95 @@ class SignInPage extends StatelessWidget {
                 borderRadius: BorderRadius.circular(20),
               ),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   // NOTE: EMAIL INPUT
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Email Address',
+                        style: blackTextStyle.copyWith(fontWeight: medium),
+                      ),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                          contentPadding: const EdgeInsets.all(12),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  // NOTE: PASSWORD INPUT
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Password',
+                        style: blackTextStyle.copyWith(fontWeight: medium),
+                      ),
+                      const SizedBox(height: 8),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(14),
+                          ),
+                          contentPadding: const EdgeInsets.all(12),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Forgot Password',
+                      style: blueTextStyle,
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  SizedBox(
+                    width: double.infinity,
+                    height: 50,
+                    child: TextButton(
+                      onPressed: () {},
+                      style: TextButton.styleFrom(
+                        backgroundColor: purpleColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(56),
+                        ),
+                      ),
+                      child: Text(
+                        'Sign in',
+                        style: whiteTextStyle.copyWith(
+                          fontWeight: semiBold,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
+              ),
+            ),
+            const SizedBox(height: 50),
+            SizedBox(
+              width: double.infinity,
+              height: 24,
+              child: TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  padding: EdgeInsets.zero,
+                ),
+                child: Text(
+                  'Create New Account',
+                  style: greyTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                ),
               ),
             ),
           ],
