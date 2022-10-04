201fb8e8f077a7c14633a5f2a35b7c3f97ae1136
seshadwi <seshaadwi@gamil.com>
2022-10-04 13:23:06 +0700

    slicing onboarding page 2

--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -34,6 +34,94 @@ class OnBoardingPage extends StatelessWidget {
                 enableInfiniteScroll: false,
               ),
             ),
+            const SizedBox(
+              height: 80,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 24,
+              ),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 22,
+                vertical: 24,
+              ),
+              decoration: BoxDecoration(
+                color: whiteColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Grow Your\nFinancial Today',
+                    style: blackTextStyle.copyWith(
+                      fontSize: 20,
+                      fontWeight: semiBold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 26),
+                  Text(
+                    'Our system is helping you to achieve a better  goal',
+                    style: greyTextStyle.copyWith(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 50),
+                  Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        width: 12,
+                        height: 12,
+                        decoration: BoxDecoration(
+                          color: blueColor,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        width: 12,
+                        height: 12,
+                        decoration: BoxDecoration(
+                          color: lightBgColor,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(right: 10),
+                        width: 12,
+                        height: 12,
+                        decoration: BoxDecoration(
+                          color: lightBgColor,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                      const Spacer(),
+                      Container(
+                        width: 150,
+                        height: 50,
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            backgroundColor: purpleColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(56),
+                            ),
+                          ),
+                          child: Text(
+                            'Continue',
+                            style: whiteTextStyle.copyWith(
+                              fontWeight: semiBold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
